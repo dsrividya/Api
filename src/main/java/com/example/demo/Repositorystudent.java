497e8ca6432eef9fa1ed7808f053ec631d929026
dsrividya <134177366+dsrividya@users.noreply.github.com>
2025-03-09 23:59:56 -0500

    Spring mvc

--- src/main/java/com/example/demo/Repositorystudent.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import com.example.demo.student.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repositorystudent extends JpaRepository<Student, Integer>{
-
-
-}
